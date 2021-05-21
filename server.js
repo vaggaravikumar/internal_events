e2b9963992fcb693944cca6114e8131fcd3fd777
vaggaravikumar <vagga.ravikumar62@gmail.com>
2021-05-21 03:55:59 +0000

    changes default text

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.use(bodyParser.json());
 // from a cloud data store
 const mockEvents = {
     events: [
-        { title: 'an event', id: 1, description: 'something really cool', location: 'Joes pizza', likes: 0 },
-        { title: 'another event', id: 2, description: 'something even cooler', location: 'Johns pizza', likes: 0 }
+        { title: 'Docker Event', id: 1, description: 'Creating Images', location: 'Joes pizza', likes: 0 },
+        { title: 'Kubernetes Event', id: 2, description: 'Kubernetes Cluster creation', location: 'Johns pizza', likes: 0 }
     ]
 };
 
